08c1959732c3fc61014796b9fb4bc9b8e28c2081
Nicholas Choma <nc2201@cs.nyu.edu>
2018-10-30 11:07:45 -0400

    Update formatting, no autograd during evaluate

--- src/main.py
+++ src/main.py
@@ -81,18 +81,23 @@ def train(
     t0 = time.time()
     logging.info("\nEpoch {}".format(i+1))
     logging.info("Learning rate: {0:.3g}".format(args.lrate))
-    # Train for one epoch
-    train_stats = train_one_epoch(net, criterion, optimizer, args, experiment_dir,
-                                 train_X, train_y, train_w)
-    # Evaluate over train, validation set
+    
+    train_stats = train_one_epoch(net,
+                                  criterion,
+                                  optimizer,
+                                  args,
+                                  experiment_dir,
+                                  train_X, train_y, train_w)
     val_stats = evaluate(net, criterion, experiment_dir, args,
                             val_X,val_y,val_w, 'Valid')
                                 
     utils.track_epoch_stats(i, args.lrate, 0, train_stats, val_stats, experiment_dir)
+
     # Update learning rate, remaining nb epochs to train
     scheduler.step(val_stats[0])
     args.lrate = optimizer.param_groups[0]['lr']
     args.nb_epochs_complete += 1
+
     # Track best model performance
     if (val_stats[0] > args.best_tpr):
       logging.warning("Best performance on valid set.")
@@ -107,61 +112,68 @@ def train(
   logging.warning("Training completed.")
 
 
-def evaluate(net, criterion, experiment_dir, args, in_X, in_y, in_w, plot_name, in_e=None, in_f=None):
-  '''
-  Evaluate network over the given set of samples
-  '''
-  net.eval()
-  epoch_loss = 0
-  # Get minibatches
-  nb_samples = len(in_X)
-  batches = utils.get_batches(nb_samples, args.batch_size)
-  nb_batches = len(batches)
-  nb_eval = nb_batches * args.batch_size
-  # Track samples by batches for scoring
-  pred_y = np.zeros((nb_eval))
-  true_y = np.zeros((nb_eval))
-  weights = np.zeros((nb_eval))
-  evt_id = []
-  f_name = []
-  # Get predictions and loss over batches
-  logging.info("Evaluating {} {} samples.".format(nb_eval,plot_name))
-  for i, batch in enumerate(batches):
-    # Wrap samples in torch Variables
-    X, y, w, adj_mask, batch_nb_nodes = utils.batch_sample(
-                                              in_X[batch],
-                                              in_y[batch],
-                                              in_w[batch]
-                                              )
-    # Make predictions over batch
-    out = net(X, adj_mask, batch_nb_nodes)
-    loss = criterion(out, y, w)
-    epoch_loss += loss.item() 
-    # Track predictions, truth, weights over batches
-    beg =     i * args.batch_size
-    end = (i+1) * args.batch_size
-    pred_y[beg:end] = out.data.cpu().numpy()
-    true_y[beg:end] = in_y[batch]
-    weights[beg:end] = in_w[batch]
-    if in_e is not None:
-      evt_id.extend(in_e[batch])
-      f_name.extend(in_f[batch])
-    # Print running loss 2 times 
-    if (((i+1) % (nb_batches//2)) == 0):
-      nb_proc = (i+1)*args.batch_size
-      logging.info("  {:5d}: {:.9f}".format(nb_proc, epoch_loss/nb_proc))
-    
-  # Score predictions, save plots, and log performance
-  epoch_loss /= nb_eval # Normalize loss
-  tpr, roc = utils.score_plot_preds(true_y, pred_y, weights,
+def evaluate(net,
+             criterion,
+             experiment_dir,
+             args,
+             in_X, in_y, in_w,
+             plot_name,
+             in_e=None,
+             in_f=None):
+    net.eval()
+    epoch_loss = 0
+    # Get minibatches
+    nb_samples = len(in_X)
+    batches = utils.get_batches(nb_samples, args.batch_size)
+    nb_batches = len(batches)
+    nb_eval = nb_batches * args.batch_size
+    # Track samples by batches for scoring
+    pred_y = np.zeros((nb_eval))
+    true_y = np.zeros((nb_eval))
+    weights = np.zeros((nb_eval))
+    evt_id = []
+    f_name = []
+    # Get predictions and loss over batches
+    logging.info("Evaluating {} {} samples.".format(nb_eval,plot_name))
+    with torch.autograd.no_grad()
+        for i, batch in enumerate(batches):
+            # Wrap samples in torch Variables
+            X, y, w, adj_mask, batch_nb_nodes = utils.batch_sample(
+                                                      in_X[batch],
+                                                      in_y[batch],
+                                                      in_w[batch]
+                                                      )
+            # Make predictions over batch
+            out = net(X, adj_mask, batch_nb_nodes)
+            loss = criterion(out, y, w)
+            epoch_loss += loss.item() 
+            # Track predictions, truth, weights over batches
+            beg =     i * args.batch_size
+            end = (i+1) * args.batch_size
+            pred_y[beg:end] = out.data.cpu().numpy()
+            true_y[beg:end] = in_y[batch]
+            weights[beg:end] = in_w[batch]
+            if in_e is not None:
+                evt_id.extend(in_e[batch])
+                f_name.extend(in_f[batch])
+            # Print running loss 2 times 
+            if (((i+1) % (nb_batches//2)) == 0):
+                nb_proc = (i+1)*args.batch_size
+                logging.info("  {:5d}: {:.9f}".format(nb_proc,
+                                                      epoch_loss/nb_proc))
+
+    # Score predictions, save plots, and log performance
+    epoch_loss /= nb_eval # Normalize loss
+    tpr, roc = utils.score_plot_preds(true_y, pred_y, weights,
                                       experiment_dir, plot_name, args.eval_tpr)
-  logging.info("{}: loss {:>.3E} -- AUC {:>.3E} -- TPR {:>.3e}".format(
+    logging.info("{}: loss {:>.3E} -- AUC {:>.3E} -- TPR {:>.3e}".format(
                                       plot_name, epoch_loss, roc, tpr))
-  if in_e is not None: utils.save_preds(evt_id, f_name, pred_y, experiment_dir)
-  # Save test numbers if evaluating on test set
-  if plot_name == TEST_NAME:
-    utils.save_test_scores(nb_eval, epoch_loss, tpr, roc, experiment_dir)
-  return (tpr, roc, epoch_loss)
+    if in_e is not None:
+        utils.save_preds(evt_id, f_name, pred_y, experiment_dir)
+    # Save test numbers if evaluating on test set
+    if plot_name == TEST_NAME:
+        utils.save_test_scores(nb_eval, epoch_loss, tpr, roc, experiment_dir)
+    return (tpr, roc, epoch_loss)
 
 
 def main():
@@ -169,9 +181,7 @@ def main():
   spatial_dims=[0,1,2]
   args = utils.read_args()
 
-  # Get path to experiment directory
   experiment_dir = utils.get_experiment_dir(args.name, args.run)
-  # Create experiment directory and csv file if not found
   utils.initialize_experiment_if_needed(experiment_dir, args.evaluate)
   # Logger will print to stdout and logfile
   utils.initialize_logger(experiment_dir)
@@ -186,8 +196,6 @@ def main():
       args.nb_epochs_complete = 0 # Track in case training interrupted
       utils.save_args(experiment_dir, args) # Save initial args
 
-  # Create model if first time running
-  # Restore model if continuing previous training or evaluating
   net = utils.create_or_restore_model(
                                     experiment_dir, 
                                     args.nb_hidden, 
@@ -200,7 +208,6 @@ def main():
     logging.warning("Training on GPU")
   criterion = nn.functional.binary_cross_entropy
   if not args.evaluate:
-    # Before loading, ensure train, val file arguments not None
     assert (args.train_file != None)
     assert (args.val_file   != None)
     train_X,train_y,train_w,_,_=utils.load_dataset(args.train_file,args.nb_train)
